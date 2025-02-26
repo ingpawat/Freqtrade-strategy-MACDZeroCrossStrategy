0b1b738b246f6c0288ec7c41b7f3bc301d60de69
ingpawat / Y <103111454+ingpawat@users.noreply.github.com>
2025-02-26 09:55:21 +0700

    Update MACDZeroCrossStrategy.py

--- MACDZeroCrossStrategy.py
+++ MACDZeroCrossStrategy.py
@@ -19,13 +19,13 @@ class MACDZeroCrossStrategy(IStrategy):
     1. **Download historical data**  
        Run the following command to fetch the required market data:
        ```
-       docker-compose run --rm freqtrade download-data --timeframes 1d --timerange=20100301-20250223
+       docker-compose run --rm freqtrade download-data --timeframes 1d --prepend --timerange 20100301-20250223 
        ```
 
     2. **Run a backtest**  
        Execute the following command to test the strategy on historical data:
        ```
-       docker-compose run --rm freqtrade backtesting --export trades -s MACDZeroCrossStrategy --timeframe 1d --timerange=20100301-20250223
+       docker-compose run --rm freqtrade backtesting --strategy MACDZeroCrossStrategy --dry-run-wallet 100 --fee 0.001 --config user_data/config.json
        ```
 
     """
